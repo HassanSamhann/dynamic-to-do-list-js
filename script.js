0261af537cd9044739b0a6a752a4b482a3ab51be
hassan <hassan700019@gmail.com>
2024-07-18 04:04:05 +0300

    end

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let addButton = document.getElementById("add-task-btn") ; 
+
+    let taskInput  = document.getElementById("task-input") ; 
+    let taskList = document.getElementById("task-list") ; 
+    console.log(taskList)
+function addTask() {
+    let taskText = taskInput.value.trim(); 
+    if(taskText === ''){
+        alert("enter a task ") ; 
+    }
+    if(taskText !== ''){
+        let li = document.createElement("li"); 
+            li.textContent = taskText ; 
+        let btn = document.createElement("button"); 
+        btn.textContent = "Remove" ; 
+        btn.className = "remove-btn" ; 
+        btn.onclick = ()=> {
+            li.remove()
+        }   
+        li.appendChild(btn); 
+        taskList.appendChild(li); 
+
+    }   
+    taskInput.value = "" ; 
+
+
+}
+taskInput.addEventListener("keypress",(e)=>{
+    if(e.key ==="Enter"){
+        addTask()
+    }
+})
+
+addButton.addEventListener("click", addTask)
+
+
+})
