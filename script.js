00b3f82bcee376671815abed04dc35b6d9be2633
hassan <hassan700019@gmail.com>
2024-07-19 17:50:16 +0300

    s

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     let addButton = document.getElementById("add-task-btn") ; 
+
+//     let taskInput  = document.getElementById("task-input") ; 
+//     let taskList = document.getElementById("task-list") ; 
+//     console.log(taskList)
+// function addTask() {
+//     let taskText = taskInput.value.trim(); 
+//     if(taskText === ''){
+//         alert("enter a task ") ; 
+//     }
+//     if(taskText !== ''){
+//         let li = document.createElement("li"); 
+//             li.textContent = taskText ; 
+//         let btn = document.createElement("button"); 
+//         btn.textContent = "Remove" ; 
+//         btn.classList.add = "remove-btn" ; 
+//         btn.onclick = ()=> {
+//             li.remove()
+//         }   
+//         li.appendChild(btn); 
+//         taskList.appendChild(li); 
+
+//     }   
+//     taskInput.value = "" ; 
 
 
+// }
+// taskInput.addEventListener("keypress",(event)=>{
+//     if(event.key ==="Enter"){
+//         addTask()
+//     }
+// })
+
+// addButton.addEventListener("click", addTask)
 
 
+// })
 document.addEventListener('DOMContentLoaded', function() {
     let addButton = document.getElementById("add-task-btn");
     let taskInput = document.getElementById("task-input");
