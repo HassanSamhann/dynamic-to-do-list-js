702a0231e3413828c2b330ba9cdaa45e386e26c4
hassan <hassan700019@gmail.com>
2024-07-19 17:47:38 +0300

    ss

--- script.js
+++ script.js
@@ -1,54 +1,18 @@
-// document.addEventListener('DOMContentLoaded', function() {
-
-//     let addButton = document.getElementById("add-task-btn") ; 
-
-//     let taskInput  = document.getElementById("task-input") ; 
-//     let taskList = document.getElementById("task-list") ; 
-//     console.log(taskList)
-// function addTask() {
-//     let taskText = taskInput.value.trim(); 
-//     if(taskText === ''){
-//         alert("enter a task ") ; 
-//     }
-//     if(taskText !== ''){
-//         let li = document.createElement("li"); 
-//             li.textContent = taskText ; 
-//         let btn = document.createElement("button"); 
-//         btn.textContent = "Remove" ; 
-//         btn.classList.add = "remove-btn" ; 
-//         btn.onclick = ()=> {
-//             li.remove()
-//         }   
-//         li.appendChild(btn); 
-//         taskList.appendChild(li); 
-
-//     }   
-//     taskInput.value = "" ; 
 
 
-// }
-// taskInput.addEventListener("keypress",(event)=>{
-//     if(event.key ==="Enter"){
-//         addTask()
-//     }
-// })
-
-// addButton.addEventListener("click", addTask)
 
 
-// })
 document.addEventListener('DOMContentLoaded', function() {
     let addButton = document.getElementById("add-task-btn");
     let taskInput = document.getElementById("task-input");
     let taskList = document.getElementById("task-list");
 
-    // Load tasks from Local Storage when the page loads
+   
     loadTasks();
 
     function loadTasks() {
         const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-        storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
-    }
+        storedTasks.forEach(taskText => addTask(taskText, false)); 
 
     function addTask(taskText, save = true) {
         let li = document.createElement("li");
